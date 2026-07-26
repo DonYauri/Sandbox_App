b811bd6d7be3e8dfb4b7d5a5b1120751c35c37dc
Brandon Kuri <brand@Someone.>
2026-07-27 05:50:36 +0800

    Set Up Express Server WithMongoDB atlas connection

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require(`dotenv`).config();
+
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+const cors = require(`cors`);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI)
+   .then(() => console.log(`MongoDB Connected`))
+   .catch((err) => console.error(`MongoDB connection error:`, err));
+
+app.get(`/api/health`, (req, res) => { 
+    res.json({status: `ok`});
+});
+
+const PORT =process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on portuhhh ${PORT}`);
+});
